9ceaea5655414481a90c166c99491eaca43e0702
irisstudios <37278563+irisstudios@users.noreply.github.com>
2024-08-16 11:47:35 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MMKV",
-            url: "https://github.com/Kyle-Ye/MMKV/releases/download/v1.3.9/MMKV.xcframework.zip",
+            url: "https://github.com/irisstudios/MMKV/releases/download/v1.3.9/MMKV.xcframework.zip",
             checksum: "7de042a4cbc75f914967258ddeb8dad362cba9d0a07f96dea42791e03a8d3516"
         ),
     ]
